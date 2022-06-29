44a5ca5ee203dafe886c8851d939cc306ba24979
Alessandro Ciavatta <alessandro.ciavatta2@studio.unibo.it>
2022-06-29 20:25:08 +0200

    added doping and bulk levels

--- material.py
+++ material.py
@@ -1,12 +1,21 @@
 import numpy as np
+import warnings
+
+'''
+doping in cm-3
+'''
+
+E_MASS = 1.457501392e-53 # cm-2 s2
+kT = 0.026   # eV @ 300 K
+PLANCK_H = 4.135667e-15  # eV s
 
 class layer:
-    def __init__(self, thickness, material) -> None:
+    def __init__(self, thickness, material):
         self.thickness = thickness
         self.material = material
 
 class material:
-    def __init__(self, epsilon, Ec, Ev, work) -> None:
+    def __init__(self, epsilon, Ec, Ev, work):
         self.epsilon = epsilon
         self.levels = {
             'Ec': Ec,
@@ -21,17 +30,31 @@ class material:
         pass
 
 class metal(material):
-    def __init__(self, work_function) -> None:
+    def __init__(self, work_function):
         super().__init__(np.inf, 0, 0, work_function)
 
 class semiconductor(material):
-    def __init__(self, epsilon, electron_affinity, doping_type=None, doping=0) -> None:
+    def __init__(self, Eg, electron_affinity, epsilon=1., effective_m_e=1, effective_m_h=None, doping_type=None, doping=0):
+        if effective_m_h is None:
+            effective_m_h = effective_m_e
+        
+        Nc = 2*(2*np.pi*E_MASS*effective_m_e*kT/PLANCK_H**2)**1.5
+        Nv = 2*(2*np.pi*E_MASS*effective_m_h*kT/PLANCK_H**2)**1.5
+        ni = np.sqrt(Nc*Nv*np.exp(Eg/kT))
+        
         if doping_type == 'n':
-            pass
+            self.n = doping
+            self.p = ni**2/doping
         elif doping_type == 'p':
-            pass
+            self.p = doping
+            self.n = ni**2/doping
         else:
-            pass
-        Ec = 0
-        Ev = 0        
+            if doping_type is not None:
+                warnings.warn("You have selected a doping type different than 'n' or 'p'. Assuming no doping")
+                doping_type = None
+            self.n = ni
+            self.p = ni
+        
+        Ec = kT*np.log(self.n/Nc)
+        Ev = -kT*np.log(self.p/Nv)
         super().__init__(epsilon, Ec, Ev, electron_affinity)
